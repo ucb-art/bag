82e5671ce64d51e5e29a28e069ff6322b91680b1
Bob Zhou <bob.linchuan@berkeley.edu>
2024-06-20 14:52:29 -0700

    Bumping tests

--- tests/test_start_bag.py
+++ tests/test_start_bag.py
@@ -4,11 +4,14 @@ sys.path.append('src')
 sys.path.append('pybag/_build/lib')
 sys.path.append('tests')
 
+# These are all necessary for BagProject initialization
 # TODO: I could use a fixture to add these
 import os
 cwd = os.getcwd()
 os.environ['BAG_TECH_CONFIG_DIR'] = f'{cwd}/tests/util/'
 os.environ['BAG_CONFIG_PATH'] = f'{cwd}/tests/util/bag_config.yaml'
+os.environ['BAG_WORK_DIR'] = f'{cwd}/tmp'
+os.environ['BAG_TEMP_DIR'] = f'{cwd}/tmp'
 
 from bag.core import BagProject
 from bag.util.misc import register_pdb_hook
