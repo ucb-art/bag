681a8abfdccf518e8cf77f4850751f33c778e0b2
Bob Zhou <bob.linchuan@berkeley.edu>
2024-06-20 15:53:27 -0700

    bump

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,5 +1,14 @@
+# Sanity checks
+
 def test_always_passes():
     assert True
+
+import numpy
+def test_import_numpy():
+    import numpy as np
+    a = [1, 2, 3]
+    an = np.array(a)
+    return True
 
 # def test_always_fails():
 #     assert False
